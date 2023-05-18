b198f9e9355664abb6c8a45e100cffbacedff925
Code-xed <ngudulu@gmail.com>
2023-05-18 13:43:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,32 @@
 import os.path
-from werkzeug.utils import secure_filename
-from flask import *
+from flask import Flask, request, render_template
 from flask_autoindex import AutoIndex
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 app.config["Uploads"] = "/"
-files_index = AutoIndex(app, "/home/bot/", add_url_rules=False)
+files_index = AutoIndex(app, "../../../" + os.path.curdir, add_url_rules=False)
 
 # Custom indexing
 @app.route('/files')
 @app.route('/files/<path:path>')
 def autoindex(path='.'):
     return files_index.render_autoindex(path)
-def li():
-	return ["apple","ball","cat"]
-
+    
 @app.route("/")
 def home():
 	return render_template("index.html")
-
-@app.route("/own")
-def own():
-	return render_template("own.html", navigation=li())
-
+	
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == "POST":
-      	file = request.files["file"]
-      	filename = secure_filename(file.filename)
-      	file.save(filename)
-      	return "fileaded successfull"
+        file = request.files["file"]
+        filename = secure_filename(file.filename)
+        file.save(filename)
+        return "Uploaded!"
     elif request.method == "GET":
-    	return render_template("upload.html")
+    	return render_template("uploader.html")
+    	
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port='5000')
